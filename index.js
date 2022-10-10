2172cf5b2459fde98a4b6ba56e08b56309fff8a2
Tyler Olson <t19.olson@gmail.com>
2022-10-10 16:54:00 -0500

    noticed second js file in utils. reworking structure

--- index.js
+++ index.js
@@ -1,100 +1,109 @@
 // TODO: Include packages needed for this application
+
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+
+
 
 // TODO: Create an array of questions for user input
-// const questions = [];
 
-inquirer
-    .prompt([
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the name of your project?',
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'What is the description of your project?',
-    },
-    {
-        type: 'input',
-        name: 'install',
-        message: 'What are the installation steps?',
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'What is the usage of your project?',
-    },
-    {
-        type: 'input',
-        name: 'credits',
-        message: 'List any additonal contributors to your project here',
-    },
-    {   type: 'list',
-        name: 'license',
-        message: 'What type of license should be used on this project?',
-        choices: ['MIT','BSD-3-Clause',]
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'What is your GitHub username?',
-    }  
+const questions = [
+    'What is the name of the project you are working on?',              //0
+    'Provide a description for the project.',                           //1
+    'What are the installation instructions for the project, if any?',  //2
+    'What are the usage instructions for the project, if any?',         //3
+    'List any additonal contributors to your project here.',            //4
+    'What type of license should be applied to this project?',          //5
+    'Enter your github username here.',                                 //6
+    'Eneter your email address here'                                    //7
 
-    ])
-    .then((response) => {
-        const content = generateReadme(response);
+];
 
-        fs.writeFile('README.md', content, (err) => 
-            err ? console.log(err) : console.log('README generated successfully')
-        );
-        })
+
 
 
 // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-const generateReadme = ({title, description, install, usage, credits, license, github}) => 
+function writeToFile(response) {
+    fs.writeFile('README.md', content, (err) => 
+        err ? console.log(err) : console.log('README generated successfully'))
+}
 
-    `# ${title}
-    
-    ${license}
-
-    # Table of Contents
-
-    1. [Installation] (#installation)
-    2. [Usage] (#usage) 
-    3. [Credits] (#credits)
-    4. [License] (#license)
-
-    ## Description
-    
-    ${description}
-    
-    ${github}
-    
-    INSERT SCREENSHOT IMAGE HERE
-    ![](assets/images/... )
-    
-    ## Installation
-    
-    ${install}
-    
-    ## Usage
-    
-    ${usage}
-    
-    ## Credits
-    
-    ${credits}
-
-    ## License
-    ${license}
-    `;
 
 // TODO: Create a function to initialize app
-// function init() {}
+function init() {
+    inquirer
+        .prompt([
+        {
+            type: 'input',
+
+            name: 'title',
+
+            message: questions[0],
+        },
+
+        {
+            type: 'input',
+
+            name: 'description',
+
+            message: questions[1],
+        },
+
+        {
+            type: 'input',
+
+            name: 'install',
+
+            message: questions[2],
+        },
+
+        {
+            type: 'input',
+
+            name: 'usage',
+
+            message: questions[3],
+        },
+
+        {
+            type: 'input',
+
+            name: 'credits',
+
+            message: questions[4],
+        },
+
+        {   type: 'list',
+
+            name: 'license',
+
+            message: questions[5],
+
+            choices: ['NONE','Apache 2.0', 'BSD-3', 'MIT', 'MPL 2.0', 'WTFPL']
+        },
+
+        {
+            type: 'input',
+
+            name: 'github',
+
+            message: questions[6],
+        },
+
+        {
+            type: 'input',
+
+            name: 'email',
+
+            message: questions[7],
+        }    
+    ])
+    .then((response) => {
+        writeToFile(generateMarkdown(response))
+        })
+}
 
 // Function call to initialize app
-// init();
+init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,89 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'NONE' ) {
+    return ''
+    
+  } else if (license === 'Apache') {
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+
+  } else if (license === 'BSD 3') {
+    return  `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`
+  
+  } else if (license === 'MIT') {
+    return `[![License](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+
+  } else if (license === 'MPL') {
+    return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+
+  } else ( license === 'WTFPL')
+    return `[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)`
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'NONE' ) {
+    return ''
+    
+  } else if (license === 'Apache') {
+    return `(https://opensource.org/licenses/Apache-2.0)`
 
+  } else if (license === 'BSD 3') {
+    return  `(https://opensource.org/licenses/BSD-3-Clause)`
+
+  } else if (license === 'MIT') {
+    return `(https://opensource.org/licenses/MIT)`
+
+  } else if (license === 'MPL') {
+    return `(https://opensource.org/licenses/MPL-2.0)`
+
+  } else ( license === 'WTFPL')
+    return `(http://www.wtfpl.net/about/)`
+
+}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(response) {
+  return `${response.title}
+  # ${title}
+    
+  ${license}
 
+  # Table of Contents
+
+  1. [Installation] (#installation)
+  2. [Usage] (#usage) 
+  3. [Credits] (#credits)
+  4. [License] (#license)
+
+  ## Description
+  
+  ${description}
+  
+  ${github}
+  
+  INSERT SCREENSHOT IMAGE HERE
+  ![](assets/images/... )
+  
+  ## Installation
+  
+  ${install}
+  
+  ## Usage
+  
+  ${usage}
+  
+  ## Credits
+  
+  ${credits}
+
+  ## License
+  ${license}
 `;
 }
 
